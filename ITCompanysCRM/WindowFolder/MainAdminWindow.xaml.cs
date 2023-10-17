05052393bc26dbc31f834a8d2bff842366f01def
Родион Богдановский <Donothink1@outlook.com>
2023-10-17 22:24:18 +0300

    qwe

---
+++ ITCompanysCRM/WindowFolder/MainAdminWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ITCompanysCRM.WindowFolder
+{
+    /// <summary>
+    /// Логика взаимодействия для MainAdminWindow.xaml
+    /// </summary>
+    public partial class MainAdminWindow : Window
+    {
+        public MainAdminWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ListOfUsersBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SettingsBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            bool resultMB = MBClass.QuestionMB("Вы действительно хотите выйти?");
+            if (resultMB)
+                App.Current.Shutdown();
+        }
+    }
+}
