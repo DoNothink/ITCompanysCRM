27b6c7ca52376fa6a81865a79b4be93347cf9e9c
Родион Богдановский <Donothink1@outlook.com>
2023-11-15 00:02:18 +0300

    fix delete trouble

--- ITCompanysCRM/WindowFolder/AdminFolder/AddUserWindow.xaml.cs
+++ ITCompanysCRM/WindowFolder/AdminFolder/AddUserWindow.xaml.cs
@@ -11,9 +11,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+
 
 namespace ITCompanysCRM.WindowFolder.AdminFolder
 {
